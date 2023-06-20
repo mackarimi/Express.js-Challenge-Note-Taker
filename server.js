2d9092c82341123679eccaa21cb2f2d18cb44470
mackarimi <kkarimi47@gmail.com>
2023-06-20 00:46:16 -0700

    Develop folder path fixed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const apiRouter = require("./routes/apiRoutes"); // import apiRoutes module
 const htmlRouter = require("./routes/htmlRoutes"); // import htmlRoutes module
 
 app.use(express.json()); // middleware to parse JSON data in the request body
-app.use(express.static(path.join(__dirname, "/develop/public"))); // middleware to serve static files from the '/develop/public' folder
+app.use(express.static(path.join(__dirname, "/Develop/public"))); // middleware to serve static files from the '/develop/public' folder
 app.use("/api", apiRouter); // use the apiRouter for all routes starting with '/api'
 app.use("/", htmlRouter); // use the htmlRouter for all routes starting with '/'
 
