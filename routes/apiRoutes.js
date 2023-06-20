68801c18c37dd18814c16cb6193b65140776e1fc
mackarimi <kkarimi47@gmail.com>
2023-06-19 23:44:43 -0700

    edit: index.js, apiRoutes.js, htmlRoutes.js and tested db.json uuid

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,107 @@
 const express = require("express");
-const apiRouter = express.Router();
+const router = express.Router();
 const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
-apiRouter.get("/notes", (req, res) => {
-  const dbPath = path.join(__dirname, "..", "db", "db.json");
-  if (fs.existsSync(dbPath)) {
-    const data = fs.readFileSync(dbPath);
-    if (data.length > 0) {
-      res.json(JSON.parse(data));
-    } else {
-      res.json([]);
+const dbPath = path.join(__dirname, "..", "db", "db.json");
+
+// Handle GET request to retrieve notes
+router.get('/notes', (req, res) => {
+  // Read the notes from the database file
+  fs.readFile(dbPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Failed to retrieve notes" });
+      return;
     }
-  } else {
-    res.json([]);
-  }
+
+    // Parse the JSON data
+    const notes = JSON.parse(data);
+
+    // Send the notes as the response
+    res.json(notes);
+  });
 });
 
-module.exports = apiRouter;
+// Handle POST request to create a new note
+router.post('/notes', (req, res) => {
+  // Read the notes from the database file
+  fs.readFile(dbPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Failed to save note" });
+      return;
+    }
+
+    // Parse the JSON data
+    const notes = JSON.parse(data);
+
+    // Generate a new note ID using uuid
+    const newNoteId = uuidv4();
+
+    // Create a new note object
+    const newNote = {
+      id: newNoteId,
+      title: req.body.title,
+      text: req.body.text,
+    };
+
+    // Add the new note to the notes array
+    notes.push(newNote);
+
+    // Write the updated notes to the database file
+    fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: "Failed to save note" });
+        return;
+      }
+
+      // Send the new note as the response
+      res.json(newNote);
+    });
+  });
+});
+
+// Handle DELETE request to delete a note by ID
+router.delete('/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  // Read the notes from the database file
+  fs.readFile(dbPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Failed to delete note" });
+      return;
+    }
+
+    // Parse the JSON data
+    const notes = JSON.parse(data);
+
+    // Find the index of the note to delete
+    const noteIndex = notes.findIndex((note) => note.id === noteId);
+
+    if (noteIndex === -1) {
+      res.status(404).json({ error: "Note not found" });
+      return;
+    }
+
+    // Remove the note from the notes array
+    const deletedNote = notes.splice(noteIndex, 1)[0];
+
+    // Write the updated notes to the database file
+    fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: "Failed to delete note" });
+        return;
+      }
+
+      // Send the deleted note as the response
+      res.json(deletedNote);
+    });
+  });
+});
+
+module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,17 @@
-const express = require('express');  // importing express
-const htmlRouter = express.Router();  // creating instance of express
-const path = require('path');  // import path module for node.js
+const express = require("express"); // importing express
+const htmlRouter = express.Router(); // creating instance of express
+const path = require("path"); // import path module for node.js
 
-// when the GET api/notes route is entered like this: http://localhost:3001/notes, load the notes.html file 
-htmlRouter.get('/notes', (req, res) => {   // this is a GET request for the /notes route
-  res.sendFile(path.join(__dirname, '..', '/develop/public/notes.html'));  // notes.html is sent back to the client
+// when the GET api/notes route is entered like this: http://localhost:3001/notes, load the notes.html file
+htmlRouter.get("/notes", (req, res) => {
+  // this is a GET request for the /notes route
+  res.sendFile(path.join(__dirname, "..", "/develop/public/notes.html")); // notes.html is sent back to the client
 });
 
 // when the GET api url is entered like this: http://localhost:3001, load the index file
-htmlRouter.get('*', (req, res) => {   // this is a GET request for * (wildcard); if no other routes are specified with the api url, then the index.html is returned to the client
-    res.sendFile(path.join(__dirname, '..', '/develop/public/index.html'));  // index.html is sent back to the client
-    });
+htmlRouter.get("*", (req, res) => {
+  // this is a GET request for * (wildcard); if no other routes are specified with the api url, then the index.html is returned to the client
+  res.sendFile(path.join(__dirname, "..", "/develop/public/index.html")); // index.html is sent back to the client
+});
 
-module.exports = htmlRouter;  // this allows the htmlRouter routes to be exported to other files for usage (see server.js)
+module.exports = htmlRouter; // this allows the htmlRouter routes to be exported to other files for usage (see server.js)
